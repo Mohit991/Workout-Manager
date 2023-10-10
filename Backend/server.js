9fb4266685ac988c05d9405d63017c68798ee727
Mohit Sharma <mohit94596@gmail.com>
2023-10-10 19:21:24 +0530

    Update server.js
    Added Varcel Cors

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,11 @@ const app = express()
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    origin: [https://workout-manager-1whq.varcel.com],
+    methods: ["POST", "GET"],
+    credentials: true
+))
 
 //using the workout routes written in workouts.js
 //routes will only fire if url gas /api/workouts in the front
