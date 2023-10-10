fb1116eced63f65d527bc28632f6b1e58cb04f5b
Mohit Sharma <mohit94596@gmail.com>
2023-10-10 20:01:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["https://workout-manager-alpha.vercel.app"],
+        origin: ["https://workout-manager-mp55.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
