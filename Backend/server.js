4a4f49a70a700cff490acd4167303960d005dc1b
Mohit <mohit94596@gmail.com>
2023-10-10 22:06:09 +0530

    1

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/workouts', workoutsRoutes)
 app.use('/api/user', userRoutes)
 
 //connect to DB
-mongoose.connect("mongodb+srv://mohit94596:es1512p8ad@mernapp.ziat16q.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     //listening for requests
     app.listen(4000, () => {
