b4550c94e96c5310109aa8e9a95d325ad98b86bf
Mohit <mohit94596@gmail.com>
2023-10-10 21:40:14 +0530

    deployement related

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../Models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+    return jwt.sign({_id}, "mohitsharma", {expiresIn: '3d'})
 }
 //login user
 const loginUser = async (req, res) => {
