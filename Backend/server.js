64f5269c027df2b450ddb613e0bd56de6a7c66a8
Mohit <mohit94596@gmail.com>
2023-10-10 21:32:22 +0530

    Deploment Related Changed

--- Backend/server.js
+++ Backend/server.js
@@ -29,8 +29,8 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     //listening for requests
-    app.listen(4000, () => {
-        console.log(`Connected to DB & listening on port 4000`)
+    app.listen(process.env.PORT, () => {
+        console.log(`Connected to DB & listening on port ${process.env.PORT}`)
     })
 })
 .catch((error) =>{
